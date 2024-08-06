cf0bfa3ca2c74328fee1df278c004b55d494430f
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-08-06 23:05:54 +0530

    Time: 3 ms (67.7%), Space: 45.2 MB (8.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,41 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
+      public int[] twoSum(int[] nums, int target) {
+               int[] numd = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(numd);
+        int start = 0;
+        int end= numd.length-1;
+
+        int[] ans = new int[2];
+        while(start<end){
+            if(numd[start]+numd[end]==target){
+                ans[0] = find_index(nums,numd[start],-1);
+                ans[1] = find_index(nums,numd[end],ans[0]);
+                break;
+            }
+            else if(numd[start]+numd[end]>target){
+             end--;
+            }
+            else {
+                start++;
+            }
+        }
+
+        return ans;
+    }
+     
+    private static int find_index(int [] arr,int j,int index)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+
+            if(arr[i]==j && i!=index )
+            {
+                index=i;
+                return i;
+
+            }
+        }
+        return -1;
+
     }
 }
