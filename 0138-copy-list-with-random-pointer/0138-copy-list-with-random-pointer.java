437ffb0c072fc5cef398ce7c8944c77306b9a414
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-08-08 16:33:01 +0530

    Time: 122 ms (7.92%), Space: 44.6 MB (18.18%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,38 +15,26 @@ class Node {
 
 class Solution {
        public Node copyRandomList(Node head) {
-        // your code here
+        if (head == null) {
+            return null;
+        }
+
+        HashMap<Node, Node> map = new HashMap<>();
+        Node current = head;
         
-        Node temp = new Node(-1);
-        Node Head = temp;
-        Node h = head;
-        int i = 1;
-        HashMap<Node, Integer> mp = new HashMap<>();
-        mp.put(null, 0);
-        while (h != null) {
-            mp.put(h, i);
-            i++;
-            Node node = new Node(h.val); // Changed from h.data to h.val
-            temp.next = node;
-            temp = temp.next;
-            h = h.next;
+        while (current != null) {
+            map.put(current, new Node(current.val));
+            current = current.next;
         }
-        h = head;
-        temp = Head.next;
-        while (h != null) {
-            if (mp.get(h.random) != 0) { // Changed from h.arb to h.random
-                Node placing = Head.next;
-                for (i = 1; i < mp.get(h.random); i++) { // Changed from h.arb to h.random
-                    placing = placing.next;
-                }
-                temp.random = placing; // Changed from temp.arb to temp.random
-            }
-            else {
-               temp.random = null; 
-            }
-            temp = temp.next;
-            h = h.next;
+
+        current = head;
+        while (current != null) {
+            Node copy = map.get(current);
+            copy.next = map.get(current.next);
+            copy.random = map.get(current.random);
+            current = current.next;
         }
-        return Head.next;
+
+        return map.get(head);
     }
 }
