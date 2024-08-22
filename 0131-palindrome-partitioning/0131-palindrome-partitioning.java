14e0276848b9e17e2c1afe81920c01a92a5c9cbc
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-08-23 00:18:02 +0530

    Time: 10 ms (18.75%), Space: 55.6 MB (98.7%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,28 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<String>> partition(String s) {
-     List<List<String>> result = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
         List<String> temp = new ArrayList<>();
-        helper(result, s, temp);
+        helper(result, s, 0, temp, new StringBuilder());
         return result;
     }
-    private void helper(List<List<String>> result, String s, List<String> temp) {
-        if (s.length() == 0) {
+
+    private void helper(List<List<String>> result, String s, int start, List<String> temp, StringBuilder current) {
+        if (start == s.length()) {
             result.add(new ArrayList<>(temp));  // Create a copy of temp before adding
             return;
         }
-        for (int i = 0; i < s.length(); i++) {
-            int start = 0;
-            int end = i;
-            if (isPalindrom(s.substring(start, end + 1))) {
-                temp.add(s.substring(start, end + 1));
-                helper(result, s.substring(end + 1), temp);
+        
+        for (int i = start; i < s.length(); i++) {
+            current.append(s.charAt(i));
+            if (isPalindrom(current)) {
+                temp.add(current.toString());
+                helper(result, s, i + 1, temp, new StringBuilder());  // Pass a new StringBuilder for the next recursion level
                 temp.remove(temp.size() - 1);
             }
+           // current.deleteCharAt(current.length() - 1);  // Backtrack
         }
     }
 
-    boolean isPalindrom(String s) {
-        StringBuilder str = new StringBuilder(s);
-        return s.equals(str.reverse().toString());
+    private boolean isPalindrom(StringBuilder sb) {
+        int left = 0;
+        int right = sb.length() - 1;
+        while (left < right) {
+            if (sb.charAt(left++) != sb.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
