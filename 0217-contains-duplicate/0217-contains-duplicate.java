111d888df99920fc0c99d2bcce443a9a0de118d4
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-08-29 22:27:17 +0530

    Time: 12 ms (50.71%), Space: 61.3 MB (6.1%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            if(mp.containsKey(nums[i])){
+              return true;
+            }
+            else {
+                mp.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}
