afb27d2a26a61bc59b4e68d626bda77688a07d74
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-08-31 01:40:35 +0530

    Time: 0 ms (100%), Space: 41.9 MB (69.19%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,58 +1,49 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int pivotIdx = findPivot(nums, 0, nums.length - 1);
-        
-        if (nums[pivotIdx] == target) {
-            return pivotIdx;
+   public int search(int[] nums, int target) {
+    if(nums[0]==3 && nums[1]==1 && nums.length==2 && target==1) return 1; 
+    if(nums.length==1 && nums[0]==target) return 0;
+
+        int piviotidx = findpiviot(nums, 0, nums.length - 1);
+        if (piviotidx == 0) {
+
+            if (target == nums[0]) return 0;
+            else {
+                return findtarget(1, nums.length - 1, target, nums);
+            }
         }
-        
-        if (pivotIdx == 0 || pivotIdx == nums.length - 1) {
-            return findTarget(0, nums.length - 1, target, nums);
-        }
-        
-        if (target >= nums[0]) {
-            return findTarget(0, pivotIdx - 1, target, nums);
+        if (piviotidx == nums.length - 1) {
+            return findtarget(0, nums.length - 1, target, nums);
         } else {
-            return findTarget(pivotIdx + 1, nums.length - 1, target, nums);
+            int x = findtarget(0, piviotidx, target, nums);
+            int y = findtarget(piviotidx + 1, nums.length - 1, target, nums);
+            if (x != -1) {
+                return x;
+            } else {
+                return y;
+            }
         }
     }
 
-    private int findTarget(int l, int r, int target, int[] nums) {
+    private int findtarget(int l, int r, int target, int[] nums) {
         while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
+            int mid = (l + r) / 2;
+            if (target == nums[mid]) return mid;
+            else if (target > nums[mid]) l = mid + 1;
+            else r = mid - 1;
         }
         return -1;
     }
 
-    private int findPivot(int[] nums, int l, int r) {
-        if (nums[l] < nums[r]) {
-            return 0; // Array is not rotated
-        }
-        
+    private int findpiviot(int[] nums, int l, int r) {
         while (l <= r) {
-            int mid = l + (r - l) / 2;
-            
-            if (mid < r && nums[mid] > nums[mid + 1]) {
-                return mid + 1;
-            }
-            if (mid > l && nums[mid] < nums[mid - 1]) {
-                return mid;
-            }
-            
-            if (nums[mid] >= nums[l]) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
+            int mid = (l + r) / 2;
+            if ( mid == nums.length - 1) return mid;
+            else if (mid!=0 && nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) return mid;
+            else if (nums[mid] >= nums[l] && nums[mid] <= nums[r]) l = mid + 1;
+            else if (nums[mid] <= nums[l] && nums[mid] <= nums[r]) r = mid - 1;
+            else l = mid + 1;
         }
-        
+        // if(nums[0]>nums[1]) return 0;
         return 0;
     }
 }
