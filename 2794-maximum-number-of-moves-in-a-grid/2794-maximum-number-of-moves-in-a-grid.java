17f0ae80f4127fbc0271c8f934bc4b22a3c28d98
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-10-30 11:20:09 +0530

    Time: 5 ms (100%), Space: 56.2 MB (68.29%) - LeetHub

---
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxMoves(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        
+        for (int i = 0; i < grid.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            ans = Math.max(ans, findMoves(grid, i, 0, dp));
+        }
+        return ans;
+    }
+
+    private int findMoves(int[][] grid, int i, int j, int[][] dp) {
+        if (j == grid[0].length - 1) {
+            dp[i][j] = 0;
+            return 0;
+        }
+        
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int max = 0;
+        if (i - 1 >= 0 && j + 1 < grid[0].length && grid[i - 1][j + 1] > grid[i][j]) {
+            int c = 1 + findMoves(grid, i - 1, j + 1, dp);
+            max = Math.max(max, c);
+        }
+        if (j + 1 < grid[0].length && grid[i][j + 1] > grid[i][j]) {
+            int c = 1 + findMoves(grid, i, j + 1, dp);
+            max = Math.max(max, c);
+        }
+        if (i + 1 < grid.length && j + 1 < grid[0].length && grid[i + 1][j + 1] > grid[i][j]) {
+            int c = 1 + findMoves(grid, i + 1, j + 1, dp);
+            max = Math.max(max, c);
+        }
+
+        dp[i][j] = max;
+        return dp[i][j];
+    }
+}
