030db9c89dd479e4068f608140f8de19e727f661
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-11-11 20:27:30 +0530

    Time: 9 ms (19.48%), Space: 46.4 MB (8.09%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        ArrayList<Integer> ans = topologicalSort(adj);
+        if(ans.size()!= numCourses) return new int[]{};
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
+        // Your code here
+        int[] inoder = new int[adj.size()];
+        for (int i = 0; i < adj.size(); i++) {
+            ArrayList<Integer> temp = adj.get(i);
+            for (int j : temp) {
+                inoder[j]++;
+            }
+        }
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < inoder.length; i++) {
+            if (inoder[i] == 0)
+                q.add(i);
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int num = q.remove();
+            ans.add(num);
+            for (int i : adj.get(num)) {
+                inoder[i]--;
+                if (inoder[i] == 0)
+                    q.add(i);
+            }
+        }
+        return ans;
+    }
+}
