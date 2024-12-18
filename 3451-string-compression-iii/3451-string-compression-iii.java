db99c82406df80ffbc6293fdcf85d21f27446308
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-12-18 13:35:53 +0530

    Time: 22 ms (37.47%), Space: 46.3 MB (22.15%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,29 +1,32 @@
 class Solution {
     public String compressedString(String word) {
-      if(word.length()==1){
-        return "1"+word;
-      }
-      //word = word+""+1;
-      int i =1;
-      int c =1;
-      String ans ="";
-      char temp = word.charAt(0);
-      while(i<word.length()){
-        if(word.charAt(i)!=temp){
-            ans += c+""+temp;
-            temp = word.charAt(i);
-            c = 1;
+        if (word.length() == 1) {
+            return "1" + word; // Handle single-character case
         }
-        else if(word.charAt(i)==temp && c==9){
-           ans += c+""+temp;
-           temp = word.charAt(i);
-           c = 1; 
+
+        int i = 1;
+        int c = 1;
+        StringBuilder ans = new StringBuilder(); // Use StringBuilder for efficiency
+        char temp = word.charAt(0);
+
+        while (i < word.length()) {
+            if (word.charAt(i) != temp) {
+                ans.append(c).append(temp); // Append count and character
+                temp = word.charAt(i);
+                c = 1; // Reset count
+            } else if (word.charAt(i) == temp && c == 9) {
+                ans.append(c).append(temp); // Append when count reaches 9
+                temp = word.charAt(i);
+                c = 1; // Reset count
+            } else {
+                c++; // Increment count
+            }
+            i++;
         }
-        else{
-            c++;
-        }
-        i++;
-      }  
-      return ans+c+""+temp;
+
+        // Append the last segment
+        ans.append(c).append(temp);
+
+        return ans.toString(); // Convert StringBuilder to String
     }
 }
