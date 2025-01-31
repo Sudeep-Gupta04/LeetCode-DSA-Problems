f3d75721b675941f8a5c5d204133661a8eff3dc2
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-02-01 01:59:06 +0530

    Time: 0 ms (100%), Space: 41.9 MB (14.71%) - LeetHub

--- 1065-binary-string-with-substrings-representing-1-to-n/1065-binary-string-with-substrings-representing-1-to-n.java
+++ 1065-binary-string-with-substrings-representing-1-to-n/1065-binary-string-with-substrings-representing-1-to-n.java
@@ -1,29 +1,8 @@
 class Solution {
-    public boolean queryString(String S, int N) {
-        Set<Integer> set = new HashSet<>();
-        
-        for (int i = 0; i < S.length(); i++) {
-            StringBuilder sb = new StringBuilder();
-            
-            for (int j = i; j < S.length(); j++) {
-                sb.append(S.charAt(j));
-                
-                if (sb.length() > 1 && sb.charAt(0) == '0') {
-                    continue;
-                }
-                
-                int v = Integer.parseInt(sb.toString(), 2);
-                
-                if (v > 0 && v <= N) {
-                    set.add(v);
-                }
-                
-                if (v > N) {
-                    break;
-                }
-            }
+     public boolean queryString(String s, int n) {
+        for(int i = 1; i <= n; i++) {
+            if (s.indexOf(Integer.toBinaryString(i)) == -1) return false;
         }
-        
-        return set.size() == N;
+        return true;
     }
 }
