cef0c8753b20ff1a02f2a22fa3781fbf90fea3ab
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-04-01 23:22:39 +0530

    Time: 0 ms (100%), Space: 15.9 MB (5.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,35 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-    void nextPermutation(std::vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        
+        int k = -1;
+        for (int i = n - 2; i >= 0; --i) {
+            if (nums[i] < nums[i + 1]) {
+                k = i;
+                break;
+            }
+        }
+
+        if (k == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        int l = -1;
+        for (int i = n - 1; i > k; --i) {
+            if (nums[i] > nums[k]) {
+                l = i;
+                break;
+            }
+        }
+
+        swap(nums[k], nums[l]);
+
+        reverse(nums.begin() + k + 1, nums.end());
     }
 };
