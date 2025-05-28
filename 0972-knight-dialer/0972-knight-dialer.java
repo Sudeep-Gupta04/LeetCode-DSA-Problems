ac61c1d36077c55fe16f19e7590d6d4f071ec3e8
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-05-28 13:30:54 +0530

    Time: 783 ms (5.15%), Space: 59.1 MB (5.15%) - LeetHub

--- 0972-knight-dialer/0972-knight-dialer.java
+++ 0972-knight-dialer/0972-knight-dialer.java
@@ -3,21 +3,17 @@ import java.util.*;
 class Solution {
     public int knightDialer(int n) {
         int mod = (int) 1e9 + 7;
-
-        // Define valid moves for each digit
         HashMap<Integer, List<Integer>> hm = new HashMap<>();
         hm.put(0, Arrays.asList(4, 6));
         hm.put(1, Arrays.asList(6, 8));
         hm.put(2, Arrays.asList(7, 9));
         hm.put(3, Arrays.asList(4, 8));
         hm.put(4, Arrays.asList(0, 3, 9));
-        hm.put(5, new ArrayList<>()); // 5 can't move anywhere
+        hm.put(5, new ArrayList<>());
         hm.put(6, Arrays.asList(0, 1, 7));
         hm.put(7, Arrays.asList(2, 6));
         hm.put(8, Arrays.asList(1, 3));
         hm.put(9, Arrays.asList(2, 4));
-
-        // Memoization map: key = "digit,remainingSteps"
         HashMap<String, Integer> dp = new HashMap<>();
         int ans = 0;
 
