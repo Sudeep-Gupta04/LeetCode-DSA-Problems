92a33ca198477b06740bcfe04b49d9bbd4dc1a61
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-06-12 04:34:57 +0530

    Time: 811 ms (5.91%), Space: 51.6 MB (10.13%) - LeetHub

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.java
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.java
@@ -1,38 +1,44 @@
 class Solution {
-    HashMap<Integer,HashSet<String>> hm;
+    HashMap<Integer, HashSet<String>> hm;
+
     public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
         int n = words.length;
         hm = new HashMap<>();
-        for(int i =0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             HashSet<String> hs = new HashSet<>();
-            for(int j = 0;j<words[i].length();j++){
-                String result = new StringBuilder(words[i]).replace(j, j+1, "X").toString();
-                hs.add(result);
+            char[] chars = words[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                char original = chars[j];
+                chars[j] = 'X';
+                hs.add(new String(chars));
+                chars[j] = original;
             }
-            hm.put(i,hs);
+            hm.put(i, hs);
         }
+
         int[] path = new int[n];
         int[] dp = new int[n];
-        for(int i =0;i<n;i++) path[i] = i;
-        Arrays.fill(dp,1);
-        int max = 1;
-        int maxidx = 0;
-        for(int i =1;i<n;i++){
-            for(int j = i-1;j>=0;j--){
-                if(isValid(i,j,words,groups) && dp[i]<=1+dp[j]){
-                    path[i] = j;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; i++) path[i] = i;
+
+        int max = 1, maxidx = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (isValid(i, j, words, groups) && dp[i] <= 1 + dp[j]) {
                     dp[i] = 1 + dp[j];
+                    path[i] = j;
                 }
             }
-            if(dp[i]>max){
+            if (dp[i] > max) {
                 max = dp[i];
                 maxidx = i;
             }
         }
+
         ArrayList<String> al = new ArrayList<>();
         int i = maxidx;
-        //System.out.println(Arrays.toString(path));
-        while(path[i]!=i){
+        while (path[i] != i) {
             al.add(words[i]);
             i = path[i];
         }
@@ -42,13 +48,18 @@ class Solution {
     }
 
     private boolean isValid(int i, int j, String[] words, int[] groups) {
-        boolean p1 = (groups[i]!=groups[j]);
-        boolean p2 = false;
+        if (groups[i] == groups[j]) return false;
+
         HashSet<String> hs = hm.get(j);
-        for(int k = 0;k<words[i].length();k++){
-            String result = new StringBuilder(words[i]).replace(k, k+1, "X").toString();
-            if(hs.contains(result)) p2 = true;
+        char[] chars = words[i].toCharArray();
+
+        for (int k = 0; k < chars.length; k++) {
+            char original = chars[k];
+            chars[k] = 'X';
+            if (hs.contains(new String(chars))) return true;
+            chars[k] = original;
         }
-        return (p1 && p2);
+
+        return false;
     }
 }
