08e39ecddb0f51279545110cc87a170da8fc0fba
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-06-12 03:10:37 +0530

    Time: 525 ms (7.04%), Space: 45.5 MB (5.63%) - LeetHub

---
+++ 1000-delete-columns-to-make-sorted-iii/1000-delete-columns-to-make-sorted-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+     public int minDeletionSize(String[] strs) {
+        int m = strs[0].length();
+        int n = strs.length;
+        int[] dp = new int[m];
+        Arrays.fill(dp,1);
+        for(int col = m-2;col>=0;col--){
+            for(int prevcol = col+1;prevcol<m;prevcol++){
+                if(prevcolisValid(col,prevcol,strs)){
+                    System.out.println(col+" "+prevcol+" V");
+                    dp[col] = Math.max(dp[col],1 + dp[prevcol]);
+                }else{
+                    System.out.println(col+" "+prevcol+" IV");
+                }
+            }
+        }
+        int ans = 0;
+        for(int i:dp) ans = Math.max(ans,i);
+        return m - ans;
+    }
+
+    public boolean prevcolisValid(int col, int prevcol, String[] strs){
+        int n = strs.length;
+        for(int i = 0;i<n;i++){
+            if(strs[i].charAt(col)>strs[i].charAt(prevcol)) return false;
+        }
+        return true;
+    }
+}
