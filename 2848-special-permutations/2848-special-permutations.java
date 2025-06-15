79d63f8849c7dc535769dd4dfe14f69910d616a3
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-06-15 12:23:43 +0530

    Time: 1354 ms (5.09%), Space: 69.2 MB (15.25%) - LeetHub

--- 2848-special-permutations/2848-special-permutations.java
+++ 2848-special-permutations/2848-special-permutations.java
@@ -3,21 +3,21 @@ class Solution {
     HashMap<String,Integer> hm = new HashMap<>();
     public int specialPerm(int[] nums) {
         int bit = 0;
-        return helper(bit,-1,0,nums);
+        return helper(bit,-1,nums);
     }
 
-    public int helper(int bit, int prvidx, int i,int[] nums){
-        if(i==nums.length) return 1;
+    public int helper(int bit, int prvidx,int[] nums){
+        if((1<<nums.length)-1==bit) return 1;
         String key = prvidx+" "+bit;
         if(hm.containsKey(key)) return hm.get(key);
         int ans = 0;
         for(int p = 0;p<nums.length;p++){
             if(prvidx==-1){
-                ans = (ans + helper(bit|(1<<p),p,i+1,nums))%mod;
+                ans = (ans + helper(bit|(1<<p),p,nums))%mod;
             }else{
                 int prvnum = nums[prvidx];
                 if((bit & (1<<p))==0 &&  (prvnum%nums[p]==0 || nums[p]%prvnum==0)){
-                    ans = (ans + helper(bit|(1<<p),p,i+1,nums))%mod;
+                    ans = (ans + helper(bit|(1<<p),p,nums))%mod;
                 }
             }
         }
