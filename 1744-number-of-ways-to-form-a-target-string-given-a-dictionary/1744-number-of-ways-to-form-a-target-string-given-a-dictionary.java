151d7297e59f9e77532ce004d3a9a697f23ae590
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-06-18 02:33:25 +0530

    Time: 580 ms (5.09%), Space: 139.1 MB (5.36%) - LeetHub

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -1,36 +1,40 @@
 class Solution {
-    int mod =(int)1e9+7;int n;int m;
-    HashMap<String,Integer> dp;
+    int mod = (int)1e9 + 7;
+    int n, m;
+    HashMap<String, Integer> dp;
+
     public int numWays(String[] words, String target) {
-        HashMap<Integer,HashMap<Character,Integer>> hs = new HashMap<>();
-        dp = new HashMap<>();
         this.n = words.length;
         this.m = words[0].length();
-        for(int j = 0;j<m;j++){
-            HashMap<Character,Integer> temp = new HashMap<>();
-            for(int i=0;i<n;i++){
+        dp = new HashMap<>();
+
+        int[][] hs = new int[m][26];
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
                 char ch = words[i].charAt(j);
-                temp.put(ch,temp.getOrDefault(ch,0)+1);
+                hs[j][ch - 'a']++;
             }
-            hs.put(j,temp);
         }
-        return helper(0,0,hs,target);
+
+        return helper(0, 0, hs, target);
     }
 
-    private int helper(int i, int j,HashMap<Integer,HashMap<Character,Integer>> hs , String tar) {
-        if(j>=tar.length()) return 1;
-        if(i>=m) return 0;
-        String key = i+","+j;
-        if(dp.containsKey(key)) return dp.get(key);
+    private int helper(int i, int j, int[][] hs, String tar) {
+        if (j >= tar.length()) return 1;
+        if (i >= m) return 0;
+
+        String key = i + "," + j;
+        if (dp.containsKey(key)) return dp.get(key);
+
         int ans = 0;
-        int frqAti = hs.get(i).getOrDefault(tar.charAt(j),0);
-        if(frqAti>0){
-            ans = (int)((ans + (1L*frqAti*helper(i+1,j+1,hs,tar)))%mod + helper(i+1,j,hs,tar))%mod;
+        int freqAtI = hs[i][tar.charAt(j) - 'a'];
+
+        if (freqAtI > 0) {
+            ans = (int)((ans + (1L * freqAtI * helper(i + 1, j + 1, hs, tar)) % mod) % mod);
         }
-        else{
-            ans = (ans + helper(i+1,j,hs,tar))%mod;
-        }
-        dp.put(key,ans);
+
+        ans = (ans + helper(i + 1, j, hs, tar)) % mod;
+        dp.put(key, ans);
         return ans;
     }
 }
